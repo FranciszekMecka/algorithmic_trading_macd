da2bbca7c78157be2b275cbe837b8e6e51f9f293
FranciszekMecka <franciszek.mecka@gmail.com>
2023-03-21 16:59:00 +0100

    added detection of intersections

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from data import get_data, inspect_data
 from macd import get_macd_columns, get_signal_columns
+from trading import trade_bot
 
 trade_data = get_data()
 
@@ -16,8 +17,22 @@ plt.plot(dates[27:], macd, 'g-', label='macd')
 plt.plot(dates[36:], signal, 'b-', label='signal')
 plt.xlabel('Date')
 plt.ylabel('Close value [$]')
-plt.xticks(range(0, len(dates), 100))
+plt.xticks(range(0, len(dates), 8))
 plt.legend(loc='upper left')
 plt.ticklabel_format(style='plain', axis='y', useOffset=False)
+
+bot = trade_bot()
+buy, sell = bot.get_intersects(close[36:], macd[9:], signal, dates[36:])
+
+for point in buy:
+    plt.plot(point[0], point[1], 'bx')
+
+
+for point in sell:
+    plt.plot(point[0], point[1], 'gx')
+
+
+print(buy)
+print(sell)
 
 plt.show()
--- trading.py
+++ trading.py
@@ -1,9 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from data import get_data, inspect_data
-from macd import get_macd_columns, get_signal_columns
+
 
 class trade_bot:
-    def __init__(self) -> None:
-        pass
-    
+    def __init__(self, funds: float = 1000, date: str = '') -> None:
+        self.funds = funds
+        self.starting_date = date
+
+    def get_intersects(self, stock_price: list, macd: list, signal: list, dates: list) -> list:
+        buy_intersects = []
+        sell_intersects = []
+
+        for i in range (len(dates)):
+            if macd[i] > signal[i] and macd[i-1] < signal[i-1]:
+                buy_intersects.append((dates[i], stock_price[i]))
+            elif macd[i] < signal[i] and macd[i-1] > signal[i-1]:
+                sell_intersects.append((dates[i], stock_price[i]))
+
+        return buy_intersects, sell_intersects
